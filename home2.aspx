72821957e23b16f986bc20aea40e4ad4030bb85f
SerhatYavas <yX5lWlJ0Z4PiD4s9ZAwx>
2015-11-22 08:58:49 +0200

    UI Changes

--- home2.aspx
+++ home2.aspx
@@ -2,7 +2,7 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
     <asp:UpdatePanel ID="upHome" runat="server"></asp:UpdatePanel>
 </asp:Content>
 
--- js/MasterPage.js
+++ js/MasterPage.js
@@ -1,17 +1,37 @@
-﻿$(document).ready(function () {
-    var flag = 0;
-    //Side Menu Opener
+﻿//Page Load
+var sidebar = true;
+
+$(document).ready(function () {
+    dividedWidth('#content_upHome', '.table');
+
     $(".sidebar-trigger").click(function () {
-        if (flag == 0) {
-            $(".navigation").animate({ left: '-=200px' }, 600, function () { flag = 1 });
-            $("#request").animate({ width: '+=200px' }, 100);
-            $("#placeholder").animate({ left: '-=100px' }, 100);
-        }
-        else {
-            $(".navigation").animate({ left: '+=200px' }, 600, function () { flag = 0 });
-            $("#request").animate({ width: '-=200px' }, 100);
-            $("#placeholder").animate({ left: '+=100px' }, 100);
-        }
+        slideObject('#sidebar', 200, sidebar, 'margin-left');
+        slideObject('#upRequest', 200, sidebar, 'margin-left');
+        slideObject('#placeholder', 200, sidebar, 'margin-left');
+        sidebar ? $('#placeholder').css('width', '100vw') : $('#placeholder').css('width', 'calc(100vw - 200px)');
+        dividedWidth('#content_upHome', '.table');
+        sidebar = !sidebar;
     });
 
+    $(window).resize(function () {
+        dividedWidth('#content_upHome', '.table');
+    });
 });
+
+//Width Calculation
+function dividedWidth(dividendObject, divisorObject) {
+    var dividendWidth = parseInt($(dividendObject).parent().css('width'));
+    var divisorWidth = parseInt($(divisorObject).css('width'));
+    var division = (dividendWidth / divisorWidth) >> 0;
+    $(dividendObject).css('width', (division * divisorWidth));
+}
+
+//Slide Object
+function slideObject(object, slideValue, bool, attr) {
+    var attrValue = parseInt($(object).css(attr));
+    if (bool) {
+        $(object).animate({ 'margin-left': (attrValue - slideValue) }, 600);
+    } else {
+        $(object).animate({ 'margin-left': (attrValue + slideValue) }, 600);
+    }
+}
